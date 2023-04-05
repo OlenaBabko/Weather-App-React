184ee459bdd0e5e3c77ae5abfbaf4bc341f252b6
ljolia <elena.babko@gmail.com>
2023-04-05 14:13:35 +0300

    setting future days forecast

--- src/ForecastDay.js
+++ src/ForecastDay.js
@@ -19,7 +19,7 @@ export default function ForecastDay(props) {
     }
     
     return (
-        <div>
+        <div className="forecastDays">
             <div className="Forecast_Day">{day()}</div>
             <WeatherIcon code={props.data.weather[0].icon} size={36} />
             <div className="Forecast_Temperature">
