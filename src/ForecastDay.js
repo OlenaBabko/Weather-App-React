d87c4ec8d231e435f75ed0d46fe25a01afaeb192
ljolia <elena.babko@gmail.com>
2023-04-06 14:59:26 +0300

    try to fix deploy failed

--- src/ForecastDay.js
+++ src/ForecastDay.js
@@ -3,37 +3,15 @@ import WeatherIcon from "./WeatherIcon";
 
 
 export default function ForecastDay(props) {
-    let unit = props.unit;
-
-    // function convertToFahrenheit(event) {
-    //     event.preventDefault();
-    //     setUnit("fahrenheit");
-    // }
-    // function convertToCelsius(event) {
-    //     event.preventDefault();
-    //     setUnit("celsius");
-    // }
-
-    function changeCelsiusFahrenheit(temperature) {
-        let roundedTemperature = 0;
-        if (unit === "celsius") {
-          roundedTemperature = Math.round(temperature);
-        } else {
-          roundedTemperature = Math.round((temperature * 9) / 5 + 32);
-        }
-        return `${roundedTemperature}`;
-    }   
-    // function minTemperature() {
-    //     let temperature = Math.round(props.data.temp.min);
-    //     return `${temperature}`;
-    // }
-    // function maxTemperature() {
-    //     let temperature = Math.round(props.data.temp.max);
-    //     return `${temperature}`;
-    // }
+    function minTemperature() {
+        let temperature = Math.round(props.data.temp.min);
+        return `${temperature}`;
+    }
+    function maxTemperature() {
+        let temperature = Math.round(props.data.temp.max);
+        return `${temperature}`;
+    }
     
-
-
     function day() {
         let date = new Date(props.data.dt * 1000);
         let day = date.getDay();
@@ -46,11 +24,23 @@ export default function ForecastDay(props) {
             <div className="Forecast_Day">{day()}</div>
             <WeatherIcon code={props.data.weather[0].icon} size={36} />
             <div className="Forecast_Temperature">
-                {/* <span className="Forecast_Temperature_min">{minTemperature()}°</span>
-                <span className="Forecast_Temperature_max">{maxTemperature()}°</span> */}
-                <span className="Forecast_Temperature_min">{changeCelsiusFahrenheit(props.data.temp.min)}°</span>
-                <span className="Forecast_Temperature_max">{changeCelsiusFahrenheit(props.data.temp.max)}°</span>
+                <span className="Forecast_Temperature_min">{minTemperature()}°</span>
+                <span className="Forecast_Temperature_max">{maxTemperature()}°</span>
+                {/* <span className="Forecast_Temperature_min">{changeCelsiusFahrenheit(props.data.temp.min)}°</span>
+                <span className="Forecast_Temperature_max">{changeCelsiusFahrenheit(props.data.temp.max)}°</span> */}
             </div>
         </div>
     );
 }
+
+
+
+// function changeCelsiusFahrenheit(temperature) {
+//     let roundedTemperature = 0;
+//     if (unit === "celsius") {
+//       roundedTemperature = Math.round(temperature);
+//     } else {
+//       roundedTemperature = Math.round((temperature * 9) / 5 + 32);
+//     }
+//     return `${roundedTemperature}`;
+// }
